b0d1cb8c58ee4b194af9fb6b6bcbc3bc8fb9f218
Kraziman <notkraziman@gmail.com>
2024-02-18 21:17:14 +0200

    Added Token.java to .gitignore

--- src/main/java/com/kraziman/crusaderbot3000/Token.java
+++ src/main/java/com/kraziman/crusaderbot3000/Token.java
@@ -2,5 +2,5 @@ package com.kraziman.crusaderbot3000;
 
 public class Token {
     // SET THE TOKEN OF THE BOT HERE
-    public static String TOKEN = "test token"; //testing if I used .gitignore correctly
+    public static String TOKEN = "BOT-TOKEN";
 }
